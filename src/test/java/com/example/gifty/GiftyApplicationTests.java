89a6b479e79699d6c89e5571d9c571149a5d23b6
cosmos-1885 <dmsrkql104321@gmail.com>
2024-02-27 02:23:48 +0900

    feat: 프로젝트 환경설정

---
+++ src/test/java/com/example/gifty/GiftyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gifty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
