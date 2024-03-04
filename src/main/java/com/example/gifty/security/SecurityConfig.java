13f4f6816e406a90e444c2e3e7dfb47a30baec2c
cosmos-1885 <dmsrkql104321@gmail.com>
2024-03-04 22:58:06 +0900

    feat: gson, jwt, swagger 의존성 설정

--- src/main/java/com/example/gifty/security/SecurityConfig.java
+++ src/main/java/com/example/gifty/security/SecurityConfig.java
@@ -1,24 +1,29 @@
 package com.example.gifty;
 
+import com.example.gifty.security.*;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
+
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JWTTokenProvider jwtTokenProvider;
+    private final CustomUserDetailService customUserDetailService;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -28,16 +33,21 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf((csrf) -> csrf.disable())
+                .csrf((csrf) -> csrf.disable()) // jwt를 사용하므로 csrf는 disable
                 .headers(headers -> headers
                         .frameOptions(frameOptions -> frameOptions.sameOrigin()))
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .cors((cors) -> cors.configurationSource(corsConfigurationSource()))
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .formLogin(formLogin -> formLogin.disable())
                 .httpBasic(httpBasic -> httpBasic.disable())
+                .addFilterBefore(new JWTAuthenticationFilter(jwtTokenProvider, customUserDetailService), UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling((excepionHandling) -> excepionHandling
+                        .authenticationEntryPoint(customAuthenticationEntryPoint)
+                        .accessDeniedHandler(customAccessDeniedHandler))
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/users/**").authenticated()
+                        .requestMatchers("/users/login").permitAll()
+                        .requestMatchers("/api/**").authenticated()
                         .anyRequest().permitAll());
         return http.build();
     }
