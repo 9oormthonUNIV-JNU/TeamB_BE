d5e740b7d0ec2c3dc6ef14b27759e4398a638ba9
cosmos-1885 <dmsrkql104321@gmail.com>
2024-03-05 02:45:07 +0900

    style: 주석 처리 해제

--- src/main/java/com/example/gifty/dto/UserRequestDTO.java
+++ src/main/java/com/example/gifty/dto/UserRequestDTO.java
@@ -10,29 +10,29 @@ import lombok.Setter;
 import java.util.HashMap;
 
 public class UserRequestDTO {
-//    @Getter
-//    @Setter
-//    public static class KakaoLoginDTO {
-//        @NotEmpty
-//        private Long kakaoId;
-//
-//        @NotEmpty
-//        @Pattern(regexp = "^[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$", message = "이메일을 올바르게 입력해주세요.")
-//        private String email;
-//
-//        @NotEmpty
-//        private String nickname;
-//
-//        @NotEmpty
-//        private String profileImage;
-//
-//        public User toEntity() {
-//            return User.builder()
-//                    .kakaoId(kakaoId)
-//                    .email(email)
-//                    .nickname(nickname)
-//                    .profileImage(profileImage)
-//                    .build();
-//        }
-//    }
+    @Getter
+    @Setter
+    public static class KakaoLoginDTO {
+        @NotEmpty
+        private Long kakaoId;
+
+        @NotEmpty
+        @Pattern(regexp = "^[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{2,6}$", message = "이메일을 올바르게 입력해주세요.")
+        private String email;
+
+        @NotEmpty
+        private String nickname;
+
+        @NotEmpty
+        private String profileImage;
+
+        public User toEntity() {
+            return User.builder()
+                    .kakaoId(kakaoId)
+                    .email(email)
+                    .nickname(nickname)
+                    .profileImage(profileImage)
+                    .build();
+        }
+    }
 }
